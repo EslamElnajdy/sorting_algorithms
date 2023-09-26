6e3b5096f8064a52333822930ae7b478d360104f
Eslam Mohamed <eslammhamed425@gmail.com>
2023-09-26 05:54:56 +0300

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap_ele - uses to swap the elements
+ * @x: the first ele
+ * @y: the second ele
+*/
+
+void swap_ele(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+/**
+ * bubble_sort - sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: array to be sorted
+ * @size: the size of array
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swaping;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swaping = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap_ele(&array[j], &array[j + 1]);
+				swaping = 1;
+			}
+		}
+		print_array(array, size);
+		if (swaping == 0)
+			break;
+	}
+}
